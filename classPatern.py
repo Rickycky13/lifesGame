e8db21a85e46382421660c5bb86c62d7b5332745
Aymeric Maury <aymeric.maurypro@gmail.com>
2024-03-03 14:16:08 +0100

    patern fini + fichier rle entier

--- classPatern.py
+++ classPatern.py
@@ -1,13 +1,14 @@
 class Patern:
-    def __init__(self,nom,ecart):
+    def __init__(self,ecart, nom = "glider"):
         """donne si nested, sa largeur, hauteur, a code pour l'afficher, sous la forme d'une liste"""
         self.nom = "rle/" + nom + ".rle"   #nom str du fichier ou ce citue le patern
         self.ecart = ecart
-        #any(isinstance(i, list) for i in a)
+
+    def newPatern(self,nom):
+        self.nom = "rle/" + nom + ".rle"   #nom str du fichier ou ce citue le patern
 
     def multProche(self,val):
         return (val//self.ecart) * self.ecart
-
 
     def extractPattern(self):
         f = open(self.nom,"r")
@@ -27,25 +28,29 @@ class Patern:
     def transformPatern(self):
         lst = []
         currentFloor = []
-        nbr = 1
+        nbr = 0
         for a in self.textPatern:
             if a == "$":
                 lst.append(currentFloor)
                 currentFloor = []
             elif a == 'b':
+                if nbr == 0:
+                    nbr = 1
                 currentFloor += [0 for _ in range(nbr)]
-                nbr=1
+                nbr=0
             elif a == 'o':
+                if nbr == 0:
+                    nbr = 1
                 currentFloor += [1 for _ in range(nbr)]
-                nbr=1
+                nbr=0
             else:
-                if nbr != 1:
+                if nbr != 0:
                     nbr = int(str(nbr)+a)
                 else:
                     nbr = int(a)
         lst.append(currentFloor)
-        self.listPatern = lst
-        
+        self.listPatern = lst   
+        print(f"liste : {self.listPatern}")     
 
     def addPatern(self,posX,posY,lst):
         for j in range(len(self.listPatern)):
