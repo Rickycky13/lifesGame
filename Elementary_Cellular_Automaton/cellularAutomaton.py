0f4d7f4e582904a6e6391ac17b72b55c6a52d931
Aymeric Maury <aymeric.maurypro@gmail.com>
2024-04-21 21:26:39 +0200

    modified

--- Elementary_Cellular_Automaton/cellularAutomaton.py
+++ Elementary_Cellular_Automaton/cellularAutomaton.py
@@ -2,80 +2,109 @@ from p5 import *
 from time import sleep
 
 celluleVivante = {}
+width = 1920
 ecart = 50
 lancement = False
 etage = 0
 rule = 255
 print(bin(rule)[2:])
 
-def rule0(dico):
+def rule7(dico):
     #000
     newDico = {}
     for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
         if dico[clef] == 0 and dico[clef - ecart] == 0 and dico[clef + ecart] == 0:
-            newDico[clef] == 1
-    return newDico
-
-def rule1(dico):
-    #001
-    newDico = {}
-    for clef in dico.keys():
-        if dico[clef] == 0 and dico[clef - ecart] == 0 and dico[clef + ecart] == 1:
-            newDico[clef] == 1
-    return newDico
-
-def rule2(dico):
-    #010
-    newDico = {}
-    for clef in dico.keys():
-        if dico[clef] == 1 and dico[clef - ecart] == 0 and dico[clef + ecart] == 0:
-            newDico[clef] == 1
-    return newDico
-
-def rule3(dico):
-    #011
-    newDico = {}
-    for clef in dico.keys():
-        if dico[clef] == 1 and dico[clef - ecart] == 0 and dico[clef + ecart] == 1:
-            newDico[clef] == 1
-    return newDico
-
-def rule4(dico):
-    #100
-    newDico = {}
-    for clef in dico.keys():
-        if dico[clef] == 0 and dico[clef - ecart] == 1 and dico[clef + ecart] == 0:
-            newDico[clef] == 1
-    return newDico
-
-def rule5(dico):
-    #101
-    newDico = {}
-    for clef in dico.keys():
-        if dico[clef] == 0 and dico[clef - ecart] == 1 and dico[clef + ecart] == 1:
-            newDico[clef] == 1
+            newDico[clef] = 1
     return newDico
 
 def rule6(dico):
+    #001
+    newDico = {}
+    for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
+        if dico[clef] == 0 and dico[clef - ecart] == 0 and dico[clef + ecart] == 1:
+            newDico[clef] = 1
+    return newDico
+
+def rule5(dico):
+    #010
+    newDico = {}
+    for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
+        if dico[clef] == 1 and dico[clef - ecart] == 0 and dico[clef + ecart] == 0:
+            newDico[clef] = 1
+    return newDico
+
+def rule4(dico):
+    #011
+    newDico = {}
+    for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
+        if dico[clef] == 0 and dico[clef - ecart] == 1 and dico[clef + ecart] == 1:
+            newDico[clef] = 1
+    return newDico
+
+def rule3(dico):
+    #100
+    newDico = {}
+    for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
+        if dico[clef] == 0 and dico[clef - ecart] == 1 and dico[clef + ecart] == 0:
+            newDico[clef] = 1
+    return newDico
+
+def rule2(dico):
+    #101
+    newDico = {}
+    for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
+        if dico[clef] == 0 and dico[clef - ecart] == 1 and dico[clef + ecart] == 1:
+            newDico[clef] = 1
+    return newDico
+
+def rule1(dico):
     #110
     newDico = {}
     for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
         if dico[clef] == 1 and dico[clef - ecart] == 1 and dico[clef + ecart] == 0:
-            newDico[clef] == 1
+            newDico[clef] = 1
     return newDico
 
-def rule7(dico):
+def rule0(dico):
     #111
     newDico = {}
     for clef in dico.keys():
+        if clef == -ecart or clef == multProche(width + ecart, ecart):
+            continue 
         if dico[clef] == 1 and dico[clef - ecart] == 1 and dico[clef + ecart] == 1:
-            newDico[clef] == 1
+            newDico[clef] = 1
     return newDico
 
 rules = [rule0,rule1,rule2,rule3,rule4,rule5,rule6,rule7]
 
 def multProche(val,mult):
     return (val//mult) * mult
+
+def concatenation(lst):
+    #recois une liste de dictionnaire et renvois un dictionnaire qui contient les positions de chacunes des cells vivantes
+    print(lst)
+    newDico = {}
+    for i in range(-ecart,width+ecart,ecart):
+        newDico[i] = 0
+    for dico in lst:
+        for clef,value in dico.items():
+            if value == 1:
+                newDico[clef] = 1
+    return newDico
 
 def editeur():
     global ecart
@@ -87,14 +116,15 @@ def editeur():
 
 def evolution():
     global celluleVivante, rule, rules
-    newCell = {}
+    concateneur = []
     for i,a in enumerate(bin(rule)[2:]):
         if a == "1":
-            newCell += rules[i](celluleVivante)
+            concateneur.append(rules[i](celluleVivante))
+    celluleVivante = concatenation(concateneur)
 
 def setup():
     size(800,800)
-    for i in range(0,1080,ecart):
+    for i in range(-ecart,width+ecart,ecart):
         celluleVivante[i] = 0
 
 def draw():
@@ -110,7 +140,7 @@ def draw():
         lancement = not lancement
         evolution()
         etage += ecart
-        sleep(0.1)
+        sleep(1)
 
 
 
